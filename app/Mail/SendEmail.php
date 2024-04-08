7717bc4b3b3a6c901fa459ac00642c5cdd2a5e96
mitim <mitin_a@mail.ru>
2024-04-08 16:05:29 +0300

    Пробная отправка письма

--- app/Mail/SendEmail.php
+++ app/Mail/SendEmail.php
@@ -30,7 +30,7 @@ class SendEmail extends Mailable
      */
     public function build()
     {
-        return $this->from('cc@osnovanie.info')
+        return $this->from('noreplay@volkovm3.beget.tech')
             ->view('mails.template', ['data' => $this->data]);
     }
 }
