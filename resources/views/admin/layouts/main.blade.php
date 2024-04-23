008a61f5bcc218d321c88b0247842241e9ed3ec7
Vyacheslav Volkov <69305848+volkov-vvv@users.noreply.github.com>
2024-04-23 11:16:39 +0300

    summernote полный функционал

--- resources/views/admin/layouts/main.blade.php
+++ resources/views/admin/layouts/main.blade.php
@@ -142,17 +142,17 @@
 <script>
     $(document).ready(function() {
         $('.summernote').summernote(
-            {
-                toolbar: [
-                    // [groupName, [list of button]]
-                    ['style', ['bold', 'italic', 'underline', 'clear']],
-                    ['font', ['strikethrough', 'superscript', 'subscript']],
-                    ['fontsize', ['fontsize']],
-                    ['color', ['color']],
-                    ['para', ['ul', 'ol', 'paragraph']],
-                    ['height', ['height']]
-                ]
-            }
+            // {
+            //     toolbar: [
+            //         // [groupName, [list of button]]
+            //         ['style', ['bold', 'italic', 'underline', 'clear']],
+            //         ['font', ['strikethrough', 'superscript', 'subscript']],
+            //         ['fontsize', ['fontsize']],
+            //         ['color', ['color']],
+            //         ['para', ['ul', 'ol', 'paragraph']],
+            //         ['height', ['height']]
+            //     ]
+            // }
         );
     });
     $(function () {
