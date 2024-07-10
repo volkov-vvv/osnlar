98c106fe364b0e7d340dea14e013387b8acf3a12
mitim <mitin_a@mail.ru>
2024-07-10 11:42:36 +0300

    sitemap

--- resources/views/sitemap/index.blade.php
+++ resources/views/sitemap/index.blade.php
@@ -25,11 +25,21 @@
         <lastmod>2024-04-22T15:48:07+01:00</lastmod>
         <priority>0.9</priority>
     </url>
+    <url>
+        <loc>https://osnovanie.info/login</loc>
+        <lastmod>2024-04-22T15:48:07+01:00</lastmod>
+        <priority>0.9</priority>
+    </url>
     @foreach($courses as $course)
         <url>
             <loc>https://osnovanie.info/course/{{$course->id}}</loc>
             <lastmod>{{$course->updated_at}}</lastmod>
             <priority>0.9</priority>
+        </url>
+        <url>
+            <loc>https://osnovanie.info/lid/create/{{$course->id}}</loc>
+            <lastmod>{{$course->updated_at}}</lastmod>
+            <priority>0.8</priority>
         </url>
     @endforeach
 </urlset>
