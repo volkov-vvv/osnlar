d11bb853b1028a81794a117af35b735211f5afa3
mitim <mitin_a@mail.ru>
2024-08-22 18:21:31 +0300

    Добавление индексов в таблицу lids

--- database/migrations/2024_08_22_181025_add_indexes_to_lids_table.php
+++ database/migrations/2024_08_22_181025_add_indexes_to_lids_table.php
@@ -14,7 +14,12 @@ class AddIndexesToLidsTable extends Migration
     public function up()
     {
         Schema::table('lids', function (Blueprint $table) {
-            $table->index(['course_id', 'region_id', 'status_id', 'category_id', 'agent_id', 'responsible_id']);
+            $table->index('course_id');
+            $table->index('region_id');
+            $table->index('status_id');
+            $table->index('category_id');
+            $table->index('agent_id');
+            $table->index('responsible_id');
         });
     }
 
@@ -26,7 +31,12 @@ class AddIndexesToLidsTable extends Migration
     public function down()
     {
         Schema::table('lids', function (Blueprint $table) {
-            $table->dropIndex(['course_id', 'region_id', 'status_id', 'category_id', 'agent_id', 'responsible_id']);
+            $table->dropIndex('course_id');
+            $table->dropIndex('region_id');
+            $table->dropIndex('status_id');
+            $table->dropIndex('category_id');
+            $table->dropIndex('agent_id');
+            $table->dropIndex('responsible_id');
         });
     }
 }
