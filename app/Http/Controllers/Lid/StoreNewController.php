fbfb84c90f255c6b1926ac0349944d7bd3e07fd0
mitin <mitin_a@mail.ru>
2024-10-21 12:22:46 +0300

    Всем заявкам присваиваем статус 'На 2025 год'

--- app/Http/Controllers/Lid/StoreNewController.php
+++ app/Http/Controllers/Lid/StoreNewController.php
@@ -15,16 +15,18 @@ class StoreNewController extends Controller
     public function __invoke(StoreNewRequest $request)
     {
         $data = $request->validated();
-        /* Определение, доступен ли курс в регионе. Пока проверка не нужна
+        // Определение, доступен ли курс в регионе. Пока проверка не нужна
         $links = Link::all()->where('region_id', $request->region_id)->where('course_id', $request->course_id)->last();
         if($links){
             $link = $links->link;
         }else{
             $link = '';
+        /*
             $status = Status::all()->where('code', 'not-in-region')->first();
             $data['status_id'] =  $status->id;
-        }
         */
+        }
+
 
         // Всем заявкам присваиваем статус 'На 2025 год'
         $status = Status::all()->where('title', 'На 2025 год')->first();
