89b18dfeaeda4e3fca98933276691c3ea8cca20d
mitin <mitin_a@mail.ru>
2024-12-02 17:45:51 +0300

    Заявки на платные курсы. Доработка админки

--- resources/views/admin/order/edit.blade.php
+++ resources/views/admin/order/edit.blade.php
@@ -87,4 +87,28 @@
         </div><!-- /.container-fluid -->
     </section>
 
+    <div class="modal fade" id="comment-modal" tabindex="-1" role="dialog">
+        <div class="modal-dialog modal-lg" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h4 class="modal-title">Введите комментарий</h4>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Закрыть">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+
+                </div>
+                <div class="modal-body">
+                    <div class="mb-3">
+                        <textarea name="comment-text" class="form-control" style="min-width: 100%"></textarea>
+                    </div>
+                    <div>
+                        <button type="button" id="comment-submit" class="btn btn-primary">Отправить</button>
+                        <button type="button" class="btn btn-outline-secondary" data-dismiss="modal">Закрыть</button>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+
 @endsection
