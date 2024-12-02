601fd97641e4edb6eb5ebd9c04dd8fe04875a19f
mitin <mitin_a@mail.ru>
2024-12-02 16:02:01 +0300

    Заявки на платные курсы. Отправка почты

--- resources/views/order/store.blade.php
+++ resources/views/order/store.blade.php
@@ -1,23 +1,22 @@
 @extends('layouts.main-lid')
 @section('content')
-    <div class="container mb-5">
-        <div class="row mb-3">
-            <div class="col">
-                <h1 class="text-center">Поздравляем!</h1>
-                <h4 class="text-center">Вы успешно оформили заказ на обучение по курсу: {{$course->title}}</h4>
+    <main>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-11 mx-auto">
+                    <section class="edica-404">
+                        <h3 class="page-title" data-aos="fade-up">Заказ оформлен!</h3>
+                        <h5 class="edica-404-subtitle" data-aos="fade-up" data-aos-delay="100">Поздравляем!</h5>
+                        <p class="edics-404-text" data-aos="fade-up" data-aos-delay="200">Статус заказа Вы можете увидеть в Личном кабинете.</p>
+                        <p class="edics-404-text" data-aos="fade-up" data-aos-delay="200">На указанный Вами почтовый ящик отправлено письмо с логином и паролем для входа в Личный кабинет.</p>
+                        <p class="edics-404-text" data-aos="fade-up" data-aos-delay="200">Вы всегда можете написать нам на почту, или позвонить по телефону: +7 (499) 609-60-20.</p>
+                        <a href="{{route('main.index')}}" class="btn btn-primary" data-aos="fade-up" data-aos-delay="300">На главную</a>
+                        <br><br>
+                        <p>Логин: <b>{{$userData['email']}}</b></p>
+                        <p>Пароль: <b>{{$userData['password']}}</b></p>
+                    </section>
+                </div>
             </div>
         </div>
-        <div class="row">
-            <div class="col">
-                <p>Статус заказа Вы можете увидеть в Личном кабинете.</p>
-                <h4 class="text-center">Данные аккаунта</h4>
-                <p>Логин: <b>{{$userData['email']}}</b></p>
-                <p>Пароль: <b>{{$userData['password']}}</b></p>
-            </div>
-        </div>
-
-
-
-
-    </div>
+    </main>
 @endsection
