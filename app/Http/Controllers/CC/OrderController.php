3dd34d0fa35e5bd3d717f444464c2fcb474f41dc
mitin <mitin_a@mail.ru>
2024-12-05 18:06:21 +0300

    Заявки на платные курсы. Ошибка в разделе Пользователи

--- app/Http/Controllers/CC/OrderController.php
+++ app/Http/Controllers/CC/OrderController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\CC;
 
 use App\Http\Controllers\Controller;
+use App\Models\Order;
 use Illuminate\Http\Request;
 
 class OrderController extends Controller
@@ -14,7 +15,8 @@ class OrderController extends Controller
      */
     public function index()
     {
-        //
+        $orders = Order::all();
+        return view('admin.order.index', compact('orders'));
     }
 
     /**
