2a4c5935b54403b1bdc7a3fbd3b23ec5b2ff934f
mitin <mitin_a@mail.ru>
2024-12-05 15:53:56 +0300

    Заявки на платные курсы. Ошибка в разделе Пользователи

--- app/Http/Controllers/Admin/User/IndexController.php
+++ app/Http/Controllers/Admin/User/IndexController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Admin\User;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
-use Illuminate\Http\Request;
+
 
 class IndexController extends Controller
 {
