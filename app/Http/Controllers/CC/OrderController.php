05f436441763ca96d6fc6f1b6c4ac2efe4e1d473
mitin <mitin_a@mail.ru>
2024-12-06 12:00:40 +0300

    Заявки на платные курсы. Заказы для СС

--- app/Http/Controllers/CC/OrderController.php
+++ app/Http/Controllers/CC/OrderController.php
@@ -18,7 +18,7 @@ class OrderController extends Controller
     public function index()
     {
         $orders = Order::all();
-        return view('admin.order.index', compact('orders'));
+        return view('cc.order.index', compact('orders'));
     }
 
     /**
