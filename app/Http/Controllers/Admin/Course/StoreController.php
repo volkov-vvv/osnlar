9939c3e5e6be2602bfe43b2e88b54d2515e08e25
mitin <mitin_a@mail.ru>
2025-03-20 17:11:25 +0300

    Исправление ошибки создания курса без utp

--- app/Http/Controllers/Admin/Course/StoreController.php
+++ app/Http/Controllers/Admin/Course/StoreController.php
@@ -13,7 +13,7 @@ class StoreController extends BaseController
     public function __invoke(StoreRequest $request)
     {
             $data = $request->validated();
-
+dd($data);
             $this->service->store($data);
 
             return redirect()->route('admin.course.index');
