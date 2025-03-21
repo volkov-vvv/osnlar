294c98f4d22935353b7e3a1a3ab033f66989a9d3
mitin <mitin_a@mail.ru>
2025-03-21 12:40:52 +0300

    Добавление активности у агентов

--- resources/views/admin/agent/edit.blade.php
+++ resources/views/admin/agent/edit.blade.php
@@ -21,11 +21,26 @@
                     @csrf
                     @method('PATCH')
                     <div class="mb-3">
-                        <label>ФИО автора</label>
-                        <input name="title" type="text" class="form-control" aria-describedby="Название" value="{{$agent->title}}">
-                        @error('title')
-                        <div class="text-danger">Это поле необходимо для заполнения "{{$message}}"</div>
-                        @enderror
+                        <div class="form-group">
+                            <label>ФИО агента</label>
+                            <input name="title" type="text" class="form-control" aria-describedby="Название" value="{{$agent->title}}">
+                            @error('title')
+                            <div class="text-danger">Это поле необходимо для заполнения "{{$message}}"</div>
+                            @enderror
+                        </div>
+
+                        <div class="form-group">
+                            <div class="custom-control custom-switch">
+                                <input type="hidden" name="active" value="0">
+                                <input name="active" type="checkbox" class="custom-control-input" id="customSwitch2" value="1"
+                                    {{ $agent->active == 1 ? ' checked' : '' }}
+                                >
+                                <label class="custom-control-label" for="customSwitch2">Активность</label>
+                            </div>
+                            @error('active')
+                            <div class="text-danger">{{$message}}</div>
+                            @enderror
+                        </div>
                     </div>
                     <button type="submit" class="btn btn-primary">Обновить</button>
                     <a class="btn btn-outline-secondary" href="{{route('admin.agent.index')}}">Назад</a>
