f0c6a2525e1e14d875040bb31fbae2fb2e83e3c2
mitin <mitin_a@mail.ru>
2025-04-15 15:44:37 +0300

    Добавление страницы с видео

--- resources/views/pages/video.blade.php
+++ resources/views/pages/video.blade.php
@@ -1,4 +1,32 @@
-@extends('layouts.main')
+@extends('layouts.main-services')
 @section('content')
-    <iframe width="720" height="405" src="https://rutube.ru/play/embed/9f34d19f35f14f39642ac28775c91ee5/" frameBorder="0" allow="clipboard-write; autoplay" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>
+    <div class="row pt-5 pb-5">
+        <div class="col text-center">
+            <h1>Видео</h1>
+        </div>
+    </div>
+
+    <main class="blog">
+        <div class="container">
+            <section class="featured-posts-section">
+                <div class="row">
+                    <div class="col fetured-post blog-post" data-aos="fade-right">
+                        <div class="card">
+                            <div class="card-body">
+                                <div class="row">
+                                    <div class="col">
+                                        <iframe width="720" height="405" src="https://rutube.ru/play/embed/9f34d19f35f14f39642ac28775c91ee5/" frameBorder="0" allow="clipboard-write; autoplay" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- /.card-body -->
+                        </div>
+                        <!-- /.card-->
+                    </div>
+                </div>
+            </section>
+
+        </div>
+
+    </main>
 @endsection
