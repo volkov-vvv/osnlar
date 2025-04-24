bb68303acab989fd0ae939357fdcd69e0767a17f
mitin <mitin_a@mail.ru>
2025-04-24 17:55:52 +0300

    Сотрировка курсов

--- app/Http/Controllers/Course/IndexController.php
+++ app/Http/Controllers/Course/IndexController.php
@@ -14,20 +14,20 @@ class IndexController extends Controller
         $courses = Course::where('is_published', 1)->whereNull('price')->where(function ($query) {
             $query->where('company_id', '=', 1)
                 ->orWhereNull('company_id');
-        })->get();
+        })->get()->sortDesc();
 
         $coursesS1 = Course::where('is_published', 1)->where('series', 1)->whereNull('price')->where(function ($query) {
             $query->where('company_id', '=', 1)
                 ->orWhereNull('company_id');
-        })->get();
+        })->get()->sortDesc();
         $coursesS2 = Course::where('is_published', 1)->where('series', 2)->whereNull('price')->where(function ($query) {
             $query->where('company_id', '=', 1)
                 ->orWhereNull('company_id');
-        })->get();
+        })->get()->sortDesc();
         $coursesS3 = Course::where('is_published', 1)->where('series', 3)->whereNull('price')->where(function ($query) {
             $query->where('company_id', '=', 1)
                 ->orWhereNull('company_id');
-        })->get();
+        })->get()->sortDesc();
         $pageTitle = "Курсы";
         $pageDescription = "Бесплатные курсы Учебного центра «Основание» по программам дополнительного профессионального образования в рамках реализации федерального проекта «Содействие занятости»";
         return view('course.index',compact('courses', 'pageTitle', 'pageDescription','coursesS1','coursesS2','coursesS3'));
