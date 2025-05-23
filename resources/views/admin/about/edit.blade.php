e3e900caf94f5ea66c41ac9923dce72ad9d8e0a1
Vyacheslav Volkov <69305848+volkov-vvv@users.noreply.github.com>
2025-05-23 17:29:20 +0300

    Обновление требований ДОГМ
    Страница Сведения об организации

--- resources/views/admin/about/edit.blade.php
+++ resources/views/admin/about/edit.blade.php
@@ -39,7 +39,7 @@
                         @enderror
                     </div>
                     <div class="mb-3">
-                        <label>Материально-техническое обеспечение и оснащенность</label>
+                        <label>Материально-техническое обеспечение и оснащенность. Доступная среда</label>
                         <textarea class="summernote" name="about_computers">
                             {{$about->about_computers}}
                         </textarea>
@@ -53,15 +53,6 @@
                             {{$about->about_all}}
                         </textarea>
                         @error('about_all')
-                        <div class="text-danger">{{$message}}</div>
-                        @enderror
-                    </div>
-                    <div class="mb-3">
-                        <label>Деятельность в ИТ</label>
-                        <textarea class="summernote" name="about_it">
-                            {{$about->about_it}}
-                        </textarea>
-                        @error('about_it')
                         <div class="text-danger">{{$message}}</div>
                         @enderror
                     </div>
@@ -92,6 +83,72 @@
                         <div class="text-danger">{{$message}}</div>
                         @enderror
                     </div>
+
+                    <div class="mb-3">
+                        <label>Вакантные места для приема и перевода обучающихся</label>
+                        <textarea class="summernote" name="about_admissions">
+                            {{$about->about_admissions}}
+                        </textarea>
+                        @error('about_admissions')
+                        <div class="text-danger">{{$message}}</div>
+                        @enderror
+                    </div>
+                    <div class="mb-3">
+                        <label>Организация питания в образовательной организации</label>
+                        <textarea class="summernote" name="about_catering">
+                            {{$about->about_catering}}
+                        </textarea>
+                        @error('about_catering')
+                        <div class="text-danger">{{$message}}</div>
+                        @enderror
+                    </div>
+                    <div class="mb-3">
+                        <label>Международной сотрудничество</label>
+                        <textarea class="summernote" name="about_global">
+                            {{$about->about_global}}
+                        </textarea>
+                        @error('about_global')
+                        <div class="text-danger">{{$message}}</div>
+                        @enderror
+                    </div>
+                    <div class="mb-3">
+                        <label>Финансово-хозяйственная деятельность</label>
+                        <textarea class="summernote" name="about_financial">
+                            {{$about->about_financial}}
+                        </textarea>
+                        @error('about_financial')
+                        <div class="text-danger">{{$message}}</div>
+                        @enderror
+                    </div>
+                    <div class="mb-3">
+                        <label>Платные образовательные услуги</label>
+                        <textarea class="summernote" name="about_tuition">
+                            {{$about->about_tuition}}
+                        </textarea>
+                        @error('about_tuition')
+                        <div class="text-danger">{{$message}}</div>
+                        @enderror
+                    </div>
+                    <div class="mb-3">
+                        <label>Образование</label>
+                        <textarea class="summernote" name="about_education">
+                            {{$about->about_education}}
+                        </textarea>
+                        @error('about_education')
+                        <div class="text-danger">{{$message}}</div>
+                        @enderror
+                    </div>
+                    <div class="mb-3">
+                        <label>Стипендии и меры поддержки обучающихся</label>
+                        <textarea class="summernote" name="about_grants">
+                            {{$about->about_grants}}
+                        </textarea>
+                        @error('about_grants')
+                        <div class="text-danger">{{$message}}</div>
+                        @enderror
+                    </div>
+
+
                     <button type="submit" class="btn btn-primary">Обновить</button>
                     <a class="btn btn-outline-secondary" href="{{route('admin.about.index')}}">Назад</a>
                 </form>
