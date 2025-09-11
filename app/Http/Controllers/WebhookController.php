2d9e93913df4f88012af19c3302740b0cd926555
mitin <mitin_a@mail.ru>
2025-09-11 15:39:52 +0300

    Webhook

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -2,12 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Helpers\Telegram;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 
 class WebhookController extends Controller
 {
-    public function index(Request $request){
+    public function index(Request $request, Telegram $telegram){
         Log::debug($request->all());
+        $chat_id = $request->input('message')['chat']['id'];
+        $from = $request->input('message')['chat']['first_name'] . ' ' . $request->input('message')['chat']['last_name'];
+        $telegram->sendMessage($chat_id, 'Привет, ' . $from);
     }
 }
