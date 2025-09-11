55dd7477ae665d82a1801ad9bb0ca1fa055fc00c
mitin <mitin_a@mail.ru>
2025-09-11 14:51:04 +0300

    Webhook

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -13,6 +13,7 @@ class VerifyCsrfToken extends Middleware
      */
     protected $except = [
         '/payments/callback',
-        '/api/lids'
+        '/api/lids',
+        '/webhook'
     ];
 }
