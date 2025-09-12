da72a4aae2e8d33fce151a1eae379e53d8fa0c2b
mitin <mitin_a@mail.ru>
2025-09-12 13:00:29 +0300

    Telegraph

--- app/Http/Telegraph/Webhook.php
+++ app/Http/Telegraph/Webhook.php
@@ -4,14 +4,15 @@
 namespace App\Http\Telegraph;
 
 
+use DefStudio\Telegraph\DTO\Message;
 use DefStudio\Telegraph\DTO\User;
 use DefStudio\Telegraph\Handlers\WebhookHandler;
 
 class Webhook extends WebhookHandler
 {
-    public function start(User $user){
+    public function start(Message $message){
 //        $username = $this->message->from()->firstName();
-        $this->message($user)->dump();
+        $this->message($message)->dd();
 //        $this->chat->html('Привет,' . $userName . '!')->send();
     }
 }
