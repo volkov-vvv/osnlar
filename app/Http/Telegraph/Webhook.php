c8ab72e0709ef200211219cfe3aeefd8af302e8d
mitin <mitin_a@mail.ru>
2025-09-12 12:20:49 +0300

    Telegraph

--- app/Http/Telegraph/Webhook.php
+++ app/Http/Telegraph/Webhook.php
@@ -9,6 +9,7 @@ use DefStudio\Telegraph\Handlers\WebhookHandler;
 class Webhook extends WebhookHandler
 {
     public function start(){
-        $this->chat->html('Привет!')->send();
+        $username = $this->message->from_user->username;
+        $this->chat->html('Привет,' . $username . '!')->send();
     }
 }
