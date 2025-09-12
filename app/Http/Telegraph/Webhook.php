fa3a3756efa07f3c59f42434dddf14c7db5ae69d
mitin <mitin_a@mail.ru>
2025-09-12 13:54:38 +0300

    Telegraph. Запрос номера телефона.

--- app/Http/Telegraph/Webhook.php
+++ app/Http/Telegraph/Webhook.php
@@ -7,16 +7,19 @@ namespace App\Http\Telegraph;
 use DefStudio\Telegraph\DTO\Message;
 use DefStudio\Telegraph\DTO\User;
 use DefStudio\Telegraph\Handlers\WebhookHandler;
+use DefStudio\Telegraph\Keyboard\ReplyButton;
+use DefStudio\Telegraph\Keyboard\ReplyKeyboard;
 use Illuminate\Support\Facades\Log;
 
 class Webhook extends WebhookHandler
 {
     public function start(){
-//        $username = $this->message->from()->firstName();
-//        Telegraph::message('test')->dump();
 
-        Log::debug($this->message->from());
+//        Log::debug($this->message->from());
 //        $this->chat->html($userName)->dd();
-        $this->chat->html('Привет, ' . $this->message->from()->firstName() . '!')->send();
+//        $this->chat->html('Привет, ' . $this->message->from()->firstName() . '!')->send();
+        $this->chat->message('Здравствуйте! Для авторизации нужно отправить свой номер телефона.')->replyKeyboard(ReplyKeyboard::make()->oneTime()->buttons([
+            ReplyButton::make('Отправить номер телефона')->requestContact()
+        ]))->send();
     }
 }
