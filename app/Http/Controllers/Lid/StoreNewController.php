26170d2c6d05f0ac8d1a0445dc2dd3aef84ed8f8
mitin <mitin_a@mail.ru>
2025-09-18 17:01:35 +0300

    Устранение ошибки при отправке кнопки в телегу

--- app/Http/Controllers/Lid/StoreNewController.php
+++ app/Http/Controllers/Lid/StoreNewController.php
@@ -12,6 +12,7 @@ use App\Models\Link;
 use App\Models\Order;
 use App\Models\Status;
 use App\Models\User;
+use DefStudio\Telegraph\Facades\Telegraph;
 use DefStudio\Telegraph\Keyboard\Keyboard;
 use DefStudio\Telegraph\Models\TelegraphChat;
 use Illuminate\Http\Request;
@@ -72,12 +73,17 @@ class StoreNewController extends Controller
         $users = User::whereIn('role', [1,3])->whereNotNull('telegraph_chat_id')->get();
         foreach ($users as $user) {
             $chat = TelegraphChat::find($user->telegraph_chat_id);
+            //dump($user);
+            //dd($chat);
             $chat->html((string)view('messages.new_lid', $data))->send();
             // Выводим кнопку для СС
+ //           dump($user);
             if($user->role == 3){
-                $chat->keyboard(
-                    Keyboard::make()->oneTime()->button('Принять заявку')->action('lidResponsible')->param('lid_id', $data['id'])
+/*
+            Telegraph::chat()->chat->keyboard(
+                    Keyboard::make()->button('Принять заявку')->action('lidResponsible')->param('lid_id', $data['id'])
                 )->send();
+*/
             }
         }
 
