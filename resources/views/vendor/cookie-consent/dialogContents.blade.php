30aee8da0aeeed43023a420cd11a8e5fb4c785da
mitin <mitin_a@mail.ru>
2025-12-18 17:35:33 +0300

    Предупреждение о cookies

--- resources/views/vendor/cookie-consent/dialogContents.blade.php
+++ resources/views/vendor/cookie-consent/dialogContents.blade.php
@@ -1,4 +1,4 @@
-<div class="cookie-consent fixed-bottom  mb-0" role="alert">
+<div class="js-cookie-consent cookie-consent fixed-bottom  mb-0" role="alert">
     <div class="row justify-content-center">
         <div class="col-md-3 rounded-2 shadow-lg alert bg-light text-center mb-5 border">
             <!-- Cookie Message -->
@@ -6,9 +6,9 @@
                 {{ __('cookie-consent::texts.message') }}
             </p>
             <!-- Btn: Accept Cookies -->
-            <a class="cookie-consent-agree btn btn-dark col-12 col-md-auto mx-auto text-white text-decoration-none mb-3" title="{{ __('cookie-consent::texts.agree') }}">
+            <button class="js-cookie-consent-agree cookie-consent__agree btn btn-dark col-12 col-md-auto mx-auto text-white text-decoration-none mb-3" title="{{ __('cookie-consent::texts.agree') }}">
                 <strong>{{ __('cookie-consent::texts.agree') }}</strong>
-            </a>
+            </button>
         </div>
     </div>
 
