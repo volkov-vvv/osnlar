0d464875638add4f45c46d40000d0fbc458a1213
Vyacheslav Volkov <69305848+volkov-vvv@users.noreply.github.com>
2026-02-12 16:24:09 +0300

    Редирект для разных ролей

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
@@ -37,4 +38,22 @@ class LoginController extends Controller
     {
         $this->middleware('guest')->except('logout');
     }
+    protected function authenticated(Request $request, $user)
+    {
+
+        if ($user->role == 1) {
+            return redirect('/admin');
+        }elseif ($user->role == 2) {
+            return redirect('/agent/report');
+        }elseif ($user->role == 3) {
+            return redirect('/cc/lid');
+        }elseif ($user->role == 10) {
+            return redirect('/user');
+        }
+        else {
+            return redirect('/');
+        }
+
+    }
+
 }
