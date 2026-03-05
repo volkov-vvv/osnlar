df76de15f6a44c7176a33ff7d62f37467fabeb7e
mitin_a <mitin_a@mail.ru>
2026-03-05 22:59:46 +0300

    Сохранение фильтров

---
+++ database/migrations/2026_03_05_224552_greate_saved_filters_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class GreateSavedFiltersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('saved_filters', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->json('filters');
+            $table->string('page_url');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('saved_filters');
+    }
+}
