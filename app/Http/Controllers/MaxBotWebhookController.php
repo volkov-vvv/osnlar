d7879c035fd0523cda2f6044b62fcae7d858387c
mitin <mitin_a@mail.ru>
2026-05-18 10:44:36 +0300

    Бот МАКС

--- app/Http/Controllers/MaxBotWebhookController.php
+++ app/Http/Controllers/MaxBotWebhookController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Services\MaxBotService;
+use App\Service\MaxBotService;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
--- app/Service/MaxBotServic.php
+++ app/Service/MaxBotServic.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Services;
+namespace App\Service;
 
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Log;
